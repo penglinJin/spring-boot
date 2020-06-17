06ecd5b578f673d18295cf66ffcda075b51ce9b0
penglinJin <834395872@qq.com>
2020-06-17 15:50:09 +0800

    First Commit of spring-boot-06-data-jpa

---
+++ src/main/java/com/jpl/springboot/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.jpl.springboot.controller;
+
+import com.jpl.springboot.entity.User;
+import com.jpl.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") Integer id){
+        User user = userRepository.findById(id).orElse(null);
+        return user;
+    }
+
+    @GetMapping("/user")
+    public User insertUser(User user){
+        User save = userRepository.save(user);
+        return save;
+    }
+
+}
